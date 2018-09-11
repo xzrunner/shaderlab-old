a73f9e9cb783b7693e086509ae300fd0c86f289d
zz <zhuguang@ejoy.com>
2018-09-12 05:47:36 +0800

    [ADDED] default node's name

--- source/NodeBuilder.cpp
+++ source/NodeBuilder.cpp
@@ -25,10 +25,11 @@
 namespace sg
 {
 
-bp::NodePtr NodeBuilder::Create(std::vector<n0::SceneNodePtr>& nodes, const std::string& type,
+bp::NodePtr NodeBuilder::Create(std::vector<n0::SceneNodePtr>& nodes,
+	                            const std::string& type, const std::string& name,
 	                            const sm::vec2& pos, bool is_default)
 {
-	auto bp_node = bp::NodeFactory::Instance()->Create(type);
+	auto bp_node = bp::NodeFactory::Instance()->Create(type, name);
 	if (!bp_node) {
 		return nullptr;
 	}
@@ -63,46 +64,46 @@ void NodeBuilder::CreateDefaultInputs(std::vector<n0::SceneNodePtr>& nodes, bp::
 	auto type = node.TypeName();
 	if (type == node::Sprite::TYPE_NAME)
 	{
-		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Sprite::ID_COL_MUL, node::Constant3::TYPE_NAME))
-			->SetValue(sm::vec3(1, 1, 1));
-		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Sprite::ID_COL_ADD, node::Constant3::TYPE_NAME))
-			->SetValue(sm::vec3(0, 0, 0));
+		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Sprite::ID_COL_MUL,
+			node::Constant3::TYPE_NAME, "mul"))->SetValue(sm::vec3(1, 1, 1));
+		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Sprite::ID_COL_ADD,
+			node::Constant3::TYPE_NAME, "add"))->SetValue(sm::vec3(0, 0, 0));
 	}
 	else if (type == node::Phong::TYPE_NAME)
 	{
 		pt3::Material mat;
-		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong::ID_AMBIENT, node::Constant3::TYPE_NAME))
-			->SetValue(mat.ambient);
-		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong::ID_DIFFUSE, node::Constant3::TYPE_NAME))
-			->SetValue(mat.diffuse);
-		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong::ID_SPECULAR, node::Constant3::TYPE_NAME))
-			->SetValue(mat.specular);
-		std::static_pointer_cast<node::Constant1>(CreateDefault(nodes, node, node::Phong::ID_SHININESS, node::Constant1::TYPE_NAME))
-			->SetValue(mat.shininess);
+		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong::ID_AMBIENT,
+			node::Constant3::TYPE_NAME, "ambient"))->SetValue(mat.ambient);
+		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong::ID_DIFFUSE,
+			node::Constant3::TYPE_NAME, "diffuse"))->SetValue(mat.diffuse);
+		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong::ID_SPECULAR,
+			node::Constant3::TYPE_NAME, "specular"))->SetValue(mat.specular);
+		std::static_pointer_cast<node::Constant1>(CreateDefault(nodes, node, node::Phong::ID_SHININESS,
+			node::Constant1::TYPE_NAME, "shininess"))->SetValue(mat.shininess);
 	}
 	else if (type == node::Phong2::TYPE_NAME)
 	{
 		// light
-		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong2::ID_LIT_POSITION, node::Constant3::TYPE_NAME))
-			->SetValue(sm::vec3(1.2f, 1.0f, 2.0f));
-		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong2::ID_LIT_AMBIENT, node::Constant3::TYPE_NAME))
-			->SetValue(sm::vec3(0.2f, 0.2f, 0.2f));
-		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong2::ID_LIT_DIFFUSE, node::Constant3::TYPE_NAME))
-			->SetValue(sm::vec3(0.5f, 0.5f, 0.5f));
-		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong2::ID_LIT_SPECULAR, node::Constant3::TYPE_NAME))
-			->SetValue(sm::vec3(1.0f, 1.0f, 1.0f));
+		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong2::ID_LIT_POSITION,
+			node::Constant3::TYPE_NAME, "lit_pos"))->SetValue(sm::vec3(1.2f, 1.0f, 2.0f));
+		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong2::ID_LIT_AMBIENT,
+			node::Constant3::TYPE_NAME, "lit_ambient"))->SetValue(sm::vec3(0.2f, 0.2f, 0.2f));
+		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong2::ID_LIT_DIFFUSE,
+			node::Constant3::TYPE_NAME, "lit_diffuse"))->SetValue(sm::vec3(0.5f, 0.5f, 0.5f));
+		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong2::ID_LIT_SPECULAR,
+			node::Constant3::TYPE_NAME, "lit_specular"))->SetValue(sm::vec3(1.0f, 1.0f, 1.0f));
 		// material
-		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong2::ID_MAT_DIFFUSE, node::Constant3::TYPE_NAME))
-			->SetValue(sm::vec3(1, 0, 0));
-		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong2::ID_MAT_SPECULAR, node::Constant3::TYPE_NAME))
-			->SetValue(sm::vec3(0, 0.5f, 0));
-		std::static_pointer_cast<node::Constant1>(CreateDefault(nodes, node, node::Phong2::ID_MAT_SHININESS, node::Constant1::TYPE_NAME))
-			->SetValue(64.0f);
-		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong2::ID_MAT_EMISSION, node::Constant3::TYPE_NAME))
-			->SetValue(sm::vec3(0, 0, 0));
+		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong2::ID_MAT_DIFFUSE,
+			node::Constant3::TYPE_NAME, "mat_diffuse"))->SetValue(sm::vec3(1, 0, 0));
+		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong2::ID_MAT_SPECULAR,
+			node::Constant3::TYPE_NAME, "mat_specular"))->SetValue(sm::vec3(0, 0.5f, 0));
+		std::static_pointer_cast<node::Constant1>(CreateDefault(nodes, node, node::Phong2::ID_MAT_SHININESS,
+			node::Constant1::TYPE_NAME, "mat_shininess"))->SetValue(64.0f);
+		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong2::ID_MAT_EMISSION,
+			node::Constant3::TYPE_NAME, "mat_emission"))->SetValue(sm::vec3(0, 0, 0));
 		// view
-		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong2::ID_VIEW_POS, node::Constant3::TYPE_NAME))
-			->SetValue(sm::vec3(1.2f, 1.0f, 2.0f));
+		std::static_pointer_cast<node::Constant3>(CreateDefault(nodes, node, node::Phong2::ID_VIEW_POS,
+			node::Constant3::TYPE_NAME, "view_pos"))->SetValue(sm::vec3(1.2f, 1.0f, 2.0f));
 	}
 	else if (type == node::Add::TYPE_NAME)
 	{
@@ -127,14 +128,14 @@ void NodeBuilder::CreateDefaultInputs(std::vector<n0::SceneNodePtr>& nodes, bp::
 }
 
 bp::NodePtr NodeBuilder::CreateDefault(std::vector<n0::SceneNodePtr>& nodes, bp::Node& parent,
-	                                   int idx, const std::string& type)
+	                                   int idx, const std::string& type, const std::string& name)
 {
 	const float dx = -65;
 
 	auto pos = bp::NodeLayout::GetPinsPos(parent, true, idx);
 	pos.x += dx;
 
-	auto child = Create(nodes, type, pos, true);
+	auto child = Create(nodes, type, name, pos, true);
 	bp::make_connecting(child->GetAllOutput()[0], parent.GetAllInput()[idx]);
 
 	return child;
