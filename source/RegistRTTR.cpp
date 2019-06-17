c569d5730dc843b79a62e360f4caf4cddc01e758
zz <zhuguang@ejoy.com>
2019-06-18 07:31:48 +0800

    [CHANGED] regist other modules rttr

--- source/RegistRTTR.cpp
+++ source/RegistRTTR.cpp
@@ -3,8 +3,6 @@
 
 #include <ee0/ReflectPropTypes.h>
 
-#include <SM_Matrix.h>
-
 #include <rttr/registration.h>
 
 RTTR_REGISTRATION
@@ -17,40 +15,12 @@ rttr::registration::class_<sg::Node>("sg::node")
 )
 ;
 
-// sm
-rttr::registration::class_<sm::mat2>("sm_mat2")
-	.property("val", &sm::mat2::x)
-;
-rttr::registration::class_<sm::mat3>("sm_mat3")
-	.property("val", &sm::mat3::x)
-;
-rttr::registration::class_<sm::mat4>("sm_mat4")
-	.property("val", &sm::mat4::x)
-;
-rttr::registration::class_<sm::bvec2>("sm_bvec2")
-	.constructor()(rttr::policy::ctor::as_object)
-	.property("x", &sm::bvec2::x)
-	.property("y", &sm::bvec2::y)
-;
-rttr::registration::class_<sm::vec2>("sm_vec2")
-	.constructor()(rttr::policy::ctor::as_object)
-	.property("x", &sm::vec2::x)
-	.property("y", &sm::vec2::y)
-;
-rttr::registration::class_<sm::vec3>("sm_vec3")
-	.constructor()(rttr::policy::ctor::as_object)
-	.property("x", &sm::vec3::x)
-	.property("y", &sm::vec3::y)
-	.property("z", &sm::vec3::z)
-;
-rttr::registration::class_<sm::vec4>("sm_vec4")
-	.constructor()(rttr::policy::ctor::as_object)
-	.property("x", &sm::vec4::x)
-	.property("y", &sm::vec4::y)
-	.property("z", &sm::vec4::z)
-	.property("w", &sm::vec4::w)
-;
+}
 
+namespace bp
+{
+extern void regist_sm_rttr();
+extern void regist_pt0_rttr();
 }
 
 namespace sg
@@ -58,6 +28,9 @@ namespace sg
 
 void regist_rttr()
 {
+    bp::regist_sm_rttr();
+    bp::regist_pt0_rttr();
+
 //	prop_types_regist_rttr();
 	nodes_regist_rttr();
 }
